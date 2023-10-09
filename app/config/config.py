b9fa295a960d197c43abfc8a07b1b346444ce8a6
Nicholas Sandomeno <nsando61@gmail.com>
2023-10-09 11:04:06 -0400

    basic config

--- app/config/config.py
+++ app/config/config.py
@@ -1,2 +1,17 @@
 import os
-import secrets
+from app.utils.environment import load_environment
+
+ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
+load_environment(ENVIRONMENT)
+
+class Config:
+    """
+        Application configuration module. requires access to environment variables.
+    """
+    ENVIRONMENT: str = os.getenv("ENVIRONMENT", "local")
+    API_PREFIX : str = "/api"
+    LOG_DIR    : str = "/var/log/hub"
+    RINGY_KEY  : str = "123password"
+
+
+config = Config()
