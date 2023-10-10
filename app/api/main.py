eee61d64b9876aa9c33aaeedef8cd6793d371706
Nicholas Sandomeno <nsando61@gmail.com>
2023-10-09 20:25:48 -0400

    reconfigure - propagate all logs

--- app/api/main.py
+++ app/api/main.py
@@ -5,11 +5,23 @@ from fastapi import FastAPI
 from app.logs.custom_logging import init_logger
 from app.utils.environment import load_environment
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.logger import logger as fastapi_logger
 
 active_env = os.environ["ENVIRONMENT"]
 load_environment(active_env)
 
 init_logger()
+gunicorn_error_logger = logging.getLogger("gunicorn.error")
+gunicorn_logger = logging.getLogger("gunicorn")
+uvicorn_access_logger = logging.getLogger("uvicorn.access")
+uvicorn_access_logger.handlers = gunicorn_error_logger.handlers
+
+fastapi_logger.handlers = gunicorn_error_logger.handlers
+
+if __name__ != "__main__":
+    fastapi_logger.setLevel(gunicorn_logger.level)
+else:
+    fastapi_logger.setLevel(logging.DEBUG)
 
 app = FastAPI(
     title="fastapi-docker",
