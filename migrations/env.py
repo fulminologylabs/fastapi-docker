b737c25cbdedf33a35e0bc088aa7c013b5a356e8
Nicholas Sandomeno <nsando61@gmail.com>
2023-10-11 18:08:58 -0400

    wip - alembic migrations

--- migrations/env.py
+++ migrations/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
+from app.database.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -17,8 +17,9 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+# TODO determine which migration pattern to use.
+target_metadata = Base.metadata
+#target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
