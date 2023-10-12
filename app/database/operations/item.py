cbb832b21dca13cc0575075d34d998939c80af33
Nicholas Sandomeno <nsando61@gmail.com>
2023-10-11 23:13:42 -0400

    crud operations

---
+++ app/database/operations/item.py
@@ -0,0 +1,13 @@
+from app.database import models
+from sqlalchemy.orm import Session
+from app.schema import Item, ItemCreate
+
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+def create_user_item(db: Session, item: ItemCreate, user_id: int):
+    db_item = models.Item(**item.model_dump(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
