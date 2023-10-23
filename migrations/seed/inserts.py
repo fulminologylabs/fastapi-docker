b62577c59bc2e9eb844b40cb7aa297f5edbd6475
Nicholas Sandomeno <nsando61@gmail.com>
2023-10-23 09:42:59 -0400

    add inserts to initial db migration

---
+++ migrations/seed/inserts.py
@@ -0,0 +1,27 @@
+### three users
+# one user with two items
+# one user with one item
+# one user with no items
+SEED_USERS = [
+    {
+        "email": "nick@fulminologylabs.co",
+        "name": "Nick",
+    },
+    {
+        "email": "nick+1item@fulminologylabs.co",
+        "name": "John",
+    },
+    {
+        "email": "nick+0items@fulminologylabs.co",
+        "name": "Jane",
+    },
+]
+
+### one items
+SEED_ITEMS = [
+    {
+        "title": "Bread",
+        "description": "This is my bread and butter.",
+        "owner_id": 1
+    }
+]
